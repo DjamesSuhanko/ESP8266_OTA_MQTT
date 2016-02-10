8001c655107a5b938dfc9c6df8a9514be1b74488
DjamesSuhanko <djames.suhanko@gmail.com>
2016-02-10 00:39:15 -0200

    logins file

---
+++ senhas.h
@@ -0,0 +1,8 @@
+struct login
+{
+    char *WEMOS       = "Wemos\0";
+    char *SSID        = "YourSSID\0";
+    char *PASS_WIFI   = "YourPass\0";
+    char *dobitaobyte = "dobitaobyte.com.br-mqtt_user\0";
+    char *PASS_BROKER = "mqtt-pass\0";
+}; 
